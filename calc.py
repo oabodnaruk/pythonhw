b4d58844f682da5e2a7bea4f656829d7c77b1a41
obodnaruk <obodnaruk@griddynamics.com>
2023-07-05 17:22:54 +0300

    refactor

--- calc.py
+++ calc.py
@@ -1,3 +1,4 @@
+
 def addition(num1, num2):
     return num1 + num2
 
@@ -15,28 +16,52 @@ def division(num1, num2):
 
 print("Welcome to the Basic Calculator Program!")
 
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+while True:
+    num1 = input("Enter the first number: ")
+    num2 = input("Enter the second number: ")
 
-print("\nSelect an operation:")
-print("1. Addition")
-print("2. Subtraction")
-print("3. Multiplication")
-print("4. Division")
+    try:
+        num1 = int(num1)
+        num2 = int(num2)
+    except ValueError:
+        print("Invalid input. Please enter numeric values.")
+        continue
 
-operation = int(input("Enter the number corresponding to your desired operation: "))
+    print("\nSelect an operation:")
+    print("1. Addition")
+    print("2. Subtraction")
+    print("3. Multiplication")
+    print("4. Division")
+    print("5. Exit")
 
-result = 0
+    operation = input("Enter the number corresponding to your desired operation: ")
 
-if operation == 1:
-    result = addition(num1, num2)
-elif operation == 2:
-    result = subtraction(num1, num2)
-elif operation == 3:
-    result = multiplication(num1, num2)
-elif operation == 4:
-    result = division(num1, num2)
-else:
-    print("Invalid operation selected.")
+    if operation == '5':
+        print("Exiting the calculator program. Goodbye!")
+        break
 
-print("The result is:", result)
+    try:
+        operation = int(operation)
+    except ValueError:
+        print("Invalid operation selected. Please enter a valid operation.")
+        continue
+
+    result = 0
+
+    if operation == 1:
+        result = addition(num1, num2)
+    elif operation == 2:
+        result = subtraction(num1, num2)
+    elif operation == 3:
+        result = multiplication(num1, num2)
+    elif operation == 4:
+        if num2 != 0:
+            result = division(num1, num2)
+        else:
+            print("Error: Cannot divide by zero.")
+            continue
+    else:
+        print("Invalid operation selected. Please try again.")
+        continue
+
+    print("The result is:", result)
